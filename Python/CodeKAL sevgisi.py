14114d724d5cdb1aff1f23af910d8c046423d781
gulbing <44496407+gulbing@users.noreply.github.com>
2019-10-23 18:54:12 +0300

    Update CodeKAL sevgisi.py

--- Python/CodeKAL sevgisi.py
+++ Python/CodeKAL sevgisi.py
@@ -4,6 +4,8 @@ print("CodeKAL'ı çok seviyorum")
 print("CodeKAL'ın bir parçası olmaktan çok mutluyum!")
 
 #GÖREV: Yukarıdaki komutların benzerin kullanarak İngilizce çıktılarını veriniz.
-
-#YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
+print("I love CodeKAL very much")
+print("I am very happy for study with CodeKAL")
+#java programlama dili phytondan daha zordu.
+YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
 # başına "#" (yorum işareti) koyarak düşüncelerinizi kodun sonuna ekleyiniz.
