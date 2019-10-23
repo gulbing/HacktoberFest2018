86f14832ba1a4970c1ca3672abf2bf7807ce93b8
gulbing <44496407+gulbing@users.noreply.github.com>
2019-10-23 19:17:40 +0300

    Update CodeKAL.java

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -8,6 +8,6 @@ public class CodeKal {
 		System.out.println("CodeKAL'ı seviyorum!");
 		System.out.println("CodeKAL'ın bir parçası olduğum için çok mutluyum.");
 
-	}
+	}System.out.println("I love CodeKAL!");
 
-}
+}System.out.println("I am very happy for study with CodeKAL.");
