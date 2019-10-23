8c848c6058aacfa506340ccddffed441d594e259
gulbing <44496407+gulbing@users.noreply.github.com>
2019-10-23 19:10:45 +0300

    Revert "Update Litre Dönüştürücü.py"

--- Python/Litre Dönüştürücü.py
+++ Python/Litre Dönüştürücü.py
@@ -1,15 +1,15 @@
 
 #GÖREV: Baş bırakılan yerleri doldurunuz:
 #Girdileri alıyoruz:
-kenar1 = int(input("1.kenarı"1.kenarı giriniz:"))
-kenar2 = int(input("2.kenarı"2.kenarı giriniz:"))
-kenar3 = int(input("3.kenarı"3.kenarı giriniz:"))
+kenar1 = int(input("1.kenarı giriniz:"))
+kenar2 = #Kodu tamamlayınız
+kenar3 = #Kodu tamamlayınız
 
 #Prizmanın hacmini cm^3 cinsinden giriniz:
-hacimCM3 = kenar1*kenar2*kenar3
+hacimCM3 = #Kodu tamamlayınız
 
 #Prizmanın hacmini litre cinsinden heaplıyoruz:
-hacimL = hacimCM3*1000
+hacimL = #Kodu tamamlayınız
 
 #Çıktıyı alıyoruz:
 print("Litre cinsinden hacmimiz: ", hacimL)
